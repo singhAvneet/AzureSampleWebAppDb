baa1a9c6da1969b1d033ab6d8d1ae9bc51f16591
Avneet <avneet_cochhar@hotmail.com>
2016-04-03 06:59:15 -0400

    init sql Db in Azure

---
+++ sampleWebAppSQLDb/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(sampleWebAppSQLDb.Startup))]
+namespace sampleWebAppSQLDb
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
